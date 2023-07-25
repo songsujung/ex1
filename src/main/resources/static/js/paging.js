29365d3b4944958bb815c9302f8d30a9b2facf88
sj <sujung033131@gmail.com>
2023-07-26 00:49:24 +0900

    0726 Board 페이징처리: paging.js, list.html

---
+++ src/main/resources/static/js/paging.js
@@ -0,0 +1,29 @@
+//paging 함수 생성
+function makePages(page, size, total) {
+
+
+    let pagingResult = "";
+
+    const startNum = (Math.ceil(page / 10) * 10) - 9;
+
+    startNum != 1 ? pagingResult += `<a href="${startNum - 1}" class="btn btn-primary"><</a>` : "";
+
+    let temp = startNum;
+
+    while(true) {
+        if(temp * 10 > total) {
+            break;
+        }
+
+        temp == page ? pagingResult += `<a href="${temp}" class="btn btn-primary active">${temp}</a>` : pagingResult += `<a href="${temp}" class="btn btn-primary">${temp}</a>`
+
+        temp++;
+    }
+
+    total % (size * 10) == 1 ? pagingResult += `<a href="${temp}" class="btn btn-primary">></a>` : "";
+
+
+    return pagingResult;
+
+
+}
