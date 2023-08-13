61898e9980052cb50f6225842a4aa12e107ddb90
sj <sujung033131@gmail.com>
2023-08-13 21:59:10 +0900

    0813 Board reply service ing

--- src/main/java/com/example/ex1/service/reply/ReplyServiceImpl.java
+++ src/main/java/com/example/ex1/service/reply/ReplyServiceImpl.java
@@ -52,24 +52,56 @@ public class ReplyServiceImpl implements ReplyService{
     // register
     @Override
     public Integer register(ReplyDTO replyDTO) {
-        return null;
+        Integer result = null;
+        int gno = replyDTO.getGno();
+
+        // 댓글일 때
+        if(gno == 0){
+            // 정상 등록 확인을 위한 count
+            // 댓글이므로 registerReply
+            int count = replyMapper.registerReply(replyDTO);
+
+            // 정상 등록 아닐 시 예외처리
+            if(count != 1){
+                throw new RuntimeException("Reply Insert Exception");
+            }
+
+            // rno값을 가져온 뒤 update
+            Integer rno = replyDTO.getRno();
+            replyMapper.updateReplyGno(rno);
+            result = rno;
+        }else{
+            // 대댓글일 때
+            int count = replyMapper.registerReplyChild(replyDTO);
+
+            // 정상 등록 아닐 시 예외처리
+            if(count != 1){
+                throw new RuntimeException("Reply Insert Exception");
+            }
+
+            result = replyDTO.getRno();
+        }
+        return result;
     }
 
     //read
     @Override
     public ReplyDTO readOne(Integer rno) {
+
         return replyMapper.readOne(rno);
     }
 
     //delete
     @Override
     public void delete(Integer rno) {
+
         replyMapper.delete(rno);
     }
 
     //modify
     @Override
     public void modify(ReplyDTO replyDTO) {
+
         replyMapper.modify(replyDTO);
     }
 }
